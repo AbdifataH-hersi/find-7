54cdf5f7b00182b50f21a2a7f7c642d8bbb30ce1
AbdifataH-hersi <mohamedabdifatah107@gmail.com>
2024-11-27 22:54:00 +0300

    find seven

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Good Luck 💪🏾
+
+let functionseven = ([1,2,3,4,5,6,7])
+
+
+for(let i = 7; i <= 7 ; i++) {
+
+    console.log(`number ${i} is found`);
+}
+
+let  findSeven = ([8, 6, 33, 100]) 
+
+
+for(let i = 1 ; i <= 100 ; i++){
+
+console.log(`there is no 7 in the array`);
+}
